8c967222544efa09edef553d8f7918caac826314
Pedro Augusto <ppedroadas@gmail.com>
2023-05-08 14:24:31 -0300

    challenge-32

--- script.js
+++ script.js
@@ -3,9 +3,7 @@
     'use strict';
 
     var app = function app(){
-    
     var dom = new DOM('[data-js="identifi"]');
-    var cars = [];
 
     return{ 
         init: function init(){
@@ -17,18 +15,17 @@
             var company = new XMLHttpRequest()
             company.open('get', '/desafio29/company.json');
             company.send('');
-            company.addEventListener('readystatechange', this.statusRequest, false);
+            company.addEventListener('readystatechange', this.statusRequestCompany, false);
         },
 
-        statusRequest: function statusRequest(){
+        statusRequestCompany: function statusRequest(){
             if(!app().isReady.call(this))
                 return;    
-            doc.querySelector('#li-name').innerHTML = dom.get(this, "name")
-            //doc.querySelector('#span-phone').innerHTML = dom.get(this, "phone")
+            doc.querySelector('#li-name').innerHTML = dom.get(this, "name");
         },
 
-        isReady: function(){
-            return this.readyState == 4 && this.status == 200;
+        isReady: function isReady(){
+            return this.readyState == 4 && this.status == 200; 
         },
 
         eventButton: function eventButton(){
@@ -40,14 +37,37 @@
             var inputsVal = []
             doc.querySelectorAll('[data-js="identifi"]').forEach(function(item){
                 inputsVal.push(item.value)
-            }); app().createElements(inputsVal);
+            }); app().methodPost(inputsVal);
         },
 
-        createElements: function createElements(inputsVal){
+        methodPost: function methodPost(inputsVal){
+            var post = new XMLHttpRequest();
+            post.open('post', 'http://localhost:3000/car');
+            post.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            post.send(`image=${inputsVal[0]}&brandModel=${inputsVal[1]}&year=${inputsVal[2]}&plate=${inputsVal[3]}&color=${inputsVal[4]}`);
+            this.methodGet()
+        },
+
+        methodGet: function methodGet(){
+            var get = new XMLHttpRequest();
+            get.open('get', 'http://localhost:3000/car');
+            get.send('');
+            get.addEventListener('readystatechange', this.statusRequestCars, false);
+        },
+
+        statusRequestCars: function statusRequestCars(){
+            if(app().isReady.call(this))
+                app().createElements(this);
+        },
+
+        createElements: function createElements(carsRequest){
             var newRow = doc.querySelector('tbody').appendChild(doc.createElement("tr"));
-            for(var index = 0; index < doc.querySelectorAll('[data-js="identifi"]').length; index++){
-                newRow.appendChild(this.returnElemDom(inputsVal[index]))
-            };
+            var Car = JSON.parse(carsRequest.responseText)[(JSON.parse(carsRequest.responseText).length) -1];
+            for (const key in Car) {
+                    const element = Car[key];
+                    newRow.appendChild(this.returnElemDom(element))
+            }
+            this.EventRemovButton((this.addRemovButton(newRow).className));
         },
 
         returnElemDom: function returnElemDom(item){
@@ -67,7 +87,31 @@
             var element = doc.createElement('img');
             element.setAttribute('src', item)
             return element;
-        }
+        },
+
+        addRemovButton: function(newRow){
+            var tdRemov = doc.createElement('td');
+            tdRemov.innerHTML = 'Remove';
+            newRow.appendChild(tdRemov);    
+            tdRemov.setAttribute('class', 'Remov' + this.createClassNumber())
+            return tdRemov
+        },
+
+        createClassNumber: function(){
+            var classNumber = Math.ceil(Math.random() * 100)
+            return classNumber
+        },
+
+        EventRemovButton: function EventRemovButton(identificator){
+            var DOMIdenti = '.'+ identificator;
+            var EventTdRemov = new DOM (DOMIdenti);
+            EventTdRemov.on(this.removeRow);
+        },
+
+        removeRow: function removeRow(){
+            this.parentNode.remove()
+        },
+
     }
 }
 
